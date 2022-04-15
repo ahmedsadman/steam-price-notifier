8503c908ee519020df1ea8733a600e6941bde58f
Ahmed Sadman <ahmedsadman.211@gmail.com>
2022-04-15 11:45:59 +0600

    increase indent space

--- steam_app.py
+++ steam_app.py
@@ -10,46 +10,43 @@ BASE_URL = 'https://store.steampowered.com/api'
 CURRENCY_CODES = utils.USD_CONVERSION_RATES.keys()
 
 class SteamApp:
-  def __init__(self, app_id):
-    self.app_id = app_id
-    self.response_content = None
-    self.name = None
-    self._get_content()
+    def __init__(self, app_id):
+        self.app_id = app_id
+        self.response_content = None
+        self.name = None
+        self._get_content()
 
-  def _get_field(self, content, field):
-    return content[self.app_id]['data'][field]
+    def _get_field(self, content, field):
+        return content[self.app_id]['data'][field]
 
-  def _get_content(self):
-    url = f'{BASE_URL}/appdetails?appids={self.app_id}'
-    res = requests.get(url)
-    self.response_content = res.json()
-    self.name = self._get_field(self.response_content, 'name')
+    def _get_content(self):
+        url = f'{BASE_URL}/appdetails?appids={self.app_id}'
+        res = requests.get(url)
+        self.response_content = res.json()
+        self.name = self._get_field(self.response_content, 'name')
     
-  def get_price(self, currency):
-    url = f'{BASE_URL}/appdetails?appids={self.app_id}&cc={currency}&filters=price_overview'
-    res = requests.get(url)
-    field = self._get_field(res.json(), 'price_overview')
-    raw_price = field['final']/100
-    return {
-      'currency': field['currency'], 
-      'formatted': field['final_formatted'], 
-      'raw': raw_price, 
-      'usd': utils.convert_to_usd(raw_price, currency)
-    }
+    def get_price(self, currency):
+        url = f'{BASE_URL}/appdetails?appids={self.app_id}&cc={currency}&filters=price_overview'
+        res = requests.get(url)
+        field = self._get_field(res.json(), 'price_overview')
+        raw_price = field['final']/100
+        return {
+            'currency': field['currency'], 
+            'formatted': field['final_formatted'], 
+            'raw': raw_price, 
+            'usd': utils.convert_to_usd(raw_price, currency)
+        }
 
-  def get_price_list(self):
-    # prices = [self.get_price(curr) for curr in CURRENCY_CODES]
-    # return sorted(prices, key=lambda d: d['usd'])
+    def get_price_list(self):
+        futures = []
+        prices = []
 
-    futures = []
-    prices = []
+        with ThreadPoolExecutor(max_workers=16) as executor:
+            for curr in CURRENCY_CODES:
+                futures.append(executor.submit(self.get_price, curr))
 
-    with ThreadPoolExecutor(max_workers=16) as executor:
-      for curr in CURRENCY_CODES:
-        futures.append(executor.submit(self.get_price, curr))
+            futures, _ = wait(futures)
+            for future in futures:
+                prices.append(future.result())
 
-      futures, _ = wait(futures)
-      for future in futures:
-        prices.append(future.result())
-
-    return sorted(prices, key=lambda d: d['usd'])
+        return sorted(prices, key=lambda d: d['usd'])
--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 USD_CONVERSION_RATES = {
-  'us': 1,
-  'in': 76.3,
-  'tr': 14.61,
-  'ar': 112.89,
-  'vn': 22901.50,
-  'kz': 457.18,
-  'id': 14385.70,
+    'us': 1,
+    'in': 76.3,
+    'tr': 14.61,
+    'ar': 112.89,
+    'vn': 22901.50,
+    'kz': 457.18,
+    'id': 14385.70,
 }
 
 def convert_to_usd(price, currency):
-  return float(format(price/USD_CONVERSION_RATES[currency], '.2f'))
+    return float(format(price/USD_CONVERSION_RATES[currency], '.2f'))
